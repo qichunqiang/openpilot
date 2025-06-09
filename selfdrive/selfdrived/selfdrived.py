38ed433f0ad412667c17f83ed301ed47013ef3f7
alan-polk <56047663+alan-polk@users.noreply.github.com>
2025-06-09 16:05:07 -0500

    (self.distance_traveled > 1500000)

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -383,7 +383,7 @@ class SelfdriveD(CruiseHelper):
     if not SIMULATION or REPLAY:
       # Not show in first 1.5 km to allow for driving out of garage. This event shows after 5 minutes
       gps_ok = self.sm.recv_frame[self.gps_location_service] > 0 and (self.sm.frame - self.sm.recv_frame[self.gps_location_service]) * DT_CTRL < 2.0
-      if not gps_ok and self.sm['livePose'].inputsOK and (self.distance_traveled > 1500):
+      if not gps_ok and self.sm['livePose'].inputsOK and (self.distance_traveled > 1500000): #changed distance to 1500 km to avoid the notice
         self.events.add(EventName.noGps)
       if gps_ok:
         self.distance_traveled = 0
