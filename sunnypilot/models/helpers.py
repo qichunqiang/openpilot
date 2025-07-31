3eaf4aa2389dbc2dc6c15386581280e29d6d043f
alan-polk <56047663+alan-polk@users.noreply.github.com>
2025-07-31 17:02:52 -0500

    Revert "updated model fetcher."
    This reverts commit 07450917dfcfb7cd8c83ea98405161b9a2a81122.

--- sunnypilot/models/helpers.py
+++ sunnypilot/models/helpers.py
@@ -1,4 +1,3 @@
-
 """
 Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
 
@@ -20,9 +19,8 @@ from openpilot.system.hardware import PC
 from openpilot.system.hardware.hw import Paths
 from pathlib import Path
 
-# see the README.md for more details on the model selector versioning
-CURRENT_SELECTOR_VERSION = 8
-REQUIRED_MIN_SELECTOR_VERSION = 6
+CURRENT_SELECTOR_VERSION = 1
+REQUIRED_MIN_SELECTOR_VERSION = 1
 
 USE_ONNX = os.getenv('USE_ONNX', PC)
 
